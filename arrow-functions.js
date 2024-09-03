9ae44d8275a1ef028dc73faa58ecbbc89f28b1a5
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-09-03 17:52:18 +0100

    arrow fuction

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,9 +6,27 @@
 // Vanilla JavaScript Function
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => a + b; also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log;(sum2)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('hello')
 
 // Returning Multiple Lines
+const returnMulitpleLines = () => (
+    '<p> This is a multiline string! </p>'
+)
+console.log(returnMulitpleLines());
